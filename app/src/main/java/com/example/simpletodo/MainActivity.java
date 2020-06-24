b4c669af9f561e7328ba66b5a99155e33b2293a4
Alex Madoff <alexmadoff@fb.com>
2020-06-23 18:31:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Have fun!");
+
+    }
+}
